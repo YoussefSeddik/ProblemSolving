8dfa68ab6b9b28708927bce2d8411aacf108732f
YoussefSeddik <youssef.seddik.s@gmail.com>
2021-11-03 18:27:41 +0000

    Sync LeetCode submission - Plus One (kotlin)

--- past-problems/problems/plus_one/solution.kt
+++ past-problems/problems/plus_one/solution.kt
@@ -1,31 +1,16 @@
 class Solution {
-
-    fun plusOne(digits: IntArray): IntArray {
-        var flag = 0
-        var sum: Int
-        var carry = 0
-        var i = digits.size - 1
-        while (i >= 0) {
-            if (flag == 0) {
-                flag = 1
-                sum = digits[i] + 1
-                digits[i] = sum % 10
-                carry = if (sum >= 10) 1 else 0
-            } else {
-                sum = digits[i] + carry
-                digits[i] = sum % 10
-                carry = if (sum >= 10) 1 else 0
-            }
-            i--
+fun plusOne(digits: IntArray): IntArray {
+    var i = digits.size - 1
+    while (i >= 0) {
+        if (digits[i] < 9) {
+            digits[i]++
+            return digits
         }
-        if (carry == 1) {
-            val resArray = IntArray(digits.size + 1)
-            resArray[0] = carry
-            for (j in 1..digits.size) {
-                resArray[j] = digits[j - 1]
-            }
-            return resArray
-        }
-        return digits
+        digits[i] = 0
+        i--
     }
+    val resultArray = IntArray(digits.size+1){0}
+    resultArray[0] = 1
+    return resultArray
+}
 }
