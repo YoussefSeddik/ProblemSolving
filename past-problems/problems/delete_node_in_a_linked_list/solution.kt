5d088e3c36bf2f37c7c219de0215948c7a1f13bb
YoussefSeddik <youssef.seddik.s@gmail.com>
2021-11-04 20:08:09 +0000

    Sync LeetCode submission - Delete Node in a Linked List (kotlin)

---
+++ past-problems/problems/delete_node_in_a_linked_list/solution.kt
@@ -0,0 +1,18 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun deleteNode(node: ListNode?) {
+        node?.next?.`val`?.let {
+            node.`val` = it
+        }
+        node?.next = node?.next?.next
+    }
+}
