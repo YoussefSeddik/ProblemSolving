13fe3880693dbc7ce1f9f60708b3152dc1d88398
YoussefSeddik <youssef.seddik.s@gmail.com>
2021-11-05 11:56:41 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (kotlin)

---
+++ past-problems/problems/maximum_depth_of_binary_tree/solution.kt
@@ -0,0 +1,18 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun maxDepth(root: TreeNode?): Int {
+        if (root == null) return 0
+        val leftDepth = maxDepth(root.left)
+        val rightDepth = maxDepth(root.right)
+        return if (leftDepth > rightDepth) leftDepth + 1 else rightDepth + 1
+    }
+}
