5248f248b48672737f37f9364ec46f600faadbfa
YoussefSeddik <youssef.seddik.s@gmail.com>
2021-11-19 19:56:41 +0000

    Sync LeetCode submission - Two Sum (kotlin)

--- past-problems/problems/two_sum/solution.kt
+++ past-problems/problems/two_sum/solution.kt
@@ -1,17 +1,15 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val res = IntArray(2)
-        for (i in nums.indices) {
-            for (j in i + 1 until nums.size) {
-                val sum = nums[i] + nums[j]
-                if (sum == target) {
-                    res[0] = i
-                    res[1] = j
-                    return res
+        val result = IntArray(2)
+        for(i in nums.indices){
+            for(j in i+1 .. nums.size - 1){
+                if(nums[j] + nums[i] == target){
+                    result[0] = i
+                    result[1] = j
                 }
+                
             }
-
         }
-        return res
+        return result
     }
 }
